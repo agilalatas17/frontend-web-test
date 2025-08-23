76492125e0f6329e9223425ee69ab1bb1ee4d4d2
agilalatas <agilalatas17@gmail.com>
2025-08-24 01:31:27 +0700

    menambahkan forwardRef di password input komponen

--- src/components/passwordInput.jsx
+++ src/components/passwordInput.jsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, forwardRef } from 'react';
+import { cn } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
 import { Eye, EyeOff } from 'lucide-react';
 
-export default function PasswordInput({ className, ...props }) {
+const PasswordInput = forwardRef(({ className, ...props }, ref) => {
   const [showPassword, setShowPassword] = useState(false);
   const type = showPassword ? 'text' : 'password';
   const Icon = showPassword ? EyeOff : Eye;
@@ -14,17 +15,23 @@ export default function PasswordInput({ className, ...props }) {
   };
 
   return (
-    <>
-      <div className="relative">
-        <Input type={type} className={`pe-9 ${className}`} {...props} />
-        <button
-          type="button"
-          onClick={handleToggle}
-          className="absolute top-1/2 -translate-y-1/2 cursor-pointer right-3"
-        >
-          <Icon className="size-4 text-slate-600" />
-        </button>
-      </div>
-    </>
+    <div className="relative">
+      <Input
+        type={type}
+        className={cn('pe-9', className)}
+        ref={ref}
+        {...props}
+      />
+      <button
+        type="button"
+        onClick={handleToggle}
+        className="absolute top-1/2 -translate-y-1/2 cursor-pointer right-3"
+      >
+        <Icon className="size-4 text-slate-600" />
+      </button>
+    </div>
   );
-}
+});
+PasswordInput.displayName = 'PasswordInput';
+
+export { PasswordInput };
