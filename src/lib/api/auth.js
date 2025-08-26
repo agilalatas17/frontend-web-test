8ce7332b01916a24108c5a06a5357061d3e6e185
agilalatas <agilalatas17@gmail.com>
2025-08-26 14:40:10 +0700

    fetch api userProfile

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -9,3 +9,13 @@ export const loginAPI = async (credentials) => {
   const res = await api.post('/auth/login', credentials);
   return res;
 };
+
+export const userProfileAPI = async (token) => {
+  const res = await api.get('/auth/profile', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return res;
+};
