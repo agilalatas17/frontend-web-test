bd72575380fdd8976d6d539fb6eb7ff19a03d1ec
agilalatas <agilalatas17@gmail.com>
2025-08-27 03:43:19 +0700

    update layout user

--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -1,7 +1,12 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
 export default function MemberLayout({ children }) {
   return (
     <>
+      <Navbar />
       <div>{children}</div>
+      <Footer />
     </>
   );
 }
