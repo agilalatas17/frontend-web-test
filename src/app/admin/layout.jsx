5b614d0228e29056c1ca05dfe6348cf908094a92
agilalatas <agilalatas17@gmail.com>
2025-08-26 13:51:22 +0700

    memisahkan variabel konstan dan memperbaiki title page agar sesuai dengan current route

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,13 +1,22 @@
+'use client';
+
 import { SidebarProvider } from '@/components/ui/sidebar';
 import SidebarComp from '@/components/SidebarComp';
 import Navbar from '@/components/Navbar';
+import { usePathname } from 'next/navigation';
+import { NAV_ITEMS } from '@/constants/nav-items';
 
 export default function AdminLayout({ children }) {
+  const pathname = usePathname();
+
+  const activeItem = NAV_ITEMS.find((item) => pathname.startsWith(item.href));
+  const titlePage = activeItem ? activeItem.title : 'Title Page';
+
   return (
     <SidebarProvider>
       <SidebarComp />
       <div className="bg-gray-100 w-full">
-        <Navbar />
+        <Navbar titlePage={titlePage} />
         <main className="p-6">{children}</main>
       </div>
     </SidebarProvider>
