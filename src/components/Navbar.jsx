ee6d4e210c031478213d89f58267f6c5b722c214
agilalatas <agilalatas17@gmail.com>
2025-08-27 03:36:16 +0700

    update UI navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,29 @@
+'use client';
+
 import Profile from '@/components/Profile';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar({ titlePage }) {
+  const role = localStorage.getItem('role');
+  console.log('CEK ROLE NAVBAR : ', role);
   return (
     <>
-      <nav className="flex flex-row justify-between items-center border-b bg-white px-6 py-2">
-        <h2 className="text-xl font-semibold leading-7 text-slate-900 capitalize">
-          {titlePage}
-        </h2>
+      <nav className="flex flex-row justify-between items-center border-b bg-white px-5 md:px-16 py-2">
+        {role === 'Admin' ? (
+          <h2 className="text-xl font-semibold leading-7 text-slate-900 capitalize">
+            {titlePage}
+          </h2>
+        ) : (
+          <Link href="/articles">
+            <Image
+              src="/assets/images/logo.png"
+              width={132}
+              height={24}
+              className="w-[122px] h-[22px] md:w-[132px] md:h-6"
+            />
+          </Link>
+        )}
 
         <Profile />
       </nav>
