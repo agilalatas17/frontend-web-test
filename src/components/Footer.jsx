b34a06dd481ad10d83abadc79a1d808720c6825d
agilalatas <agilalatas17@gmail.com>
2025-08-27 12:24:47 +0700

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <>
-      <footer className="border border-red-500 py-10 bg-primary text-white w-full text-center">
-        <div className="flex gap-x-4 justify-center">
+      <footer className="py-6 md:py-10 bg-primary text-white w-full text-center">
+        <div className="flex flex-col md:flex-row text-center gap-y-2 md:gap-x-4 justify-center items-center">
           <Image
             src="/assets/images/logo-white.png"
             alt="logo brand"
             width={132}
             height={24}
           />
-          <p>© 2025 Blog genzet, made with ❤️. All rights reserved.</p>
+          <p>© 2025 Blog genzet. All rights reserved.</p>
         </div>
       </footer>
     </>
